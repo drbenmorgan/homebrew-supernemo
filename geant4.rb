7f65d3cea9eb86c22cc8a3f3c74e217e18187fea
Ben Morgan <bmorgan.warwick@gmail.com>
2015-03-10 15:51:45 +0000

    Provide SuperNEMO specific formular for Geant4
    To allow use of an older version of Geant4 by current SuperNEMO
    software, provide decicated formula in the tap.

---
+++ geant4.rb
@@ -0,0 +1,37 @@
+class Geant4 < Formula
+  homepage "http://geant4.cern.ch"
+  url "http://geant4.cern.ch/support/source/geant4.9.6.p04.tar.gz"
+  version "9.6.4"
+  sha256 "997220a5386a43ac8f533fc7d5a8360aa1fd6338244d17deeaa583fb3a0f39fd"
+
+  option "with-gdml", "Build with GDML support"
+
+  depends_on "cmake" => :build
+  depends_on :x11 if OS.linux?
+  depends_on "drbenmorgan/supernemo/clhep"
+  depends_on "drbenmorgan/supernemo/xerces-c" if build.with? "gdml"
+
+
+  def install
+    mkdir 'geant4-build' do
+      args = std_cmake_args
+      args << "-DCMAKE_INSTALL_LIBDIR=lib"
+      args << "-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON"
+      args << "-DGEANT4_INSTALL_DATA=ON"
+      args << "-DGEANT4_USE_OPENGL_X11=ON" if OS.linux?
+      args << "-DGEANT4_USE_RAYTRACER_X11=ON" if OS.linux?
+      args << "-DGEANT4_USE_SYSTEM_CLHEP=ON"
+      args << "-DGEANT4_USE_SYSTEM_EXPAT=ON"
+      #args << "-DGEANT4_USE_SYSTEM_ZLIB=ON"
+
+      args << "-DGEANT4_USE_GDML=ON" if build.with? "gdml"
+
+      system "cmake", "../", *args
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "false"
+  end
+end
