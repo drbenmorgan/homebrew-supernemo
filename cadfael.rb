e1b4356232fcdbdf568384de27810ed89ad7ece4
Ben Morgan <bmorgan.warwick@gmail.com>
2015-03-20 16:17:46 +0000

    cadfael:
    Don't install boost-python yet as the linuxbrew formula requires patching
    for linux.

--- cadfael.rb
+++ cadfael.rb
@@ -5,7 +5,7 @@ class Cadfael < Formula
   depends_on "drbenmorgan/supernemo/cadfael-toolchain"
 
   depends_on "boost" => ["with-icu4c", "without-single", "without-static"]
-  depends_on "boost-python"
+  #depends_on "boost-python"
   depends_on "drbenmorgan/supernemo/camp" => "with-doc"
   depends_on "drbenmorgan/supernemo/clhep"
   depends_on "drbenmorgan/supernemo/geant4" => "with-gdml"
