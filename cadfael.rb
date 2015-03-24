6bbc298aa1d907f38e9fb9bf85c9ef5ee8d12530
Ben Morgan <bmorgan.warwick@gmail.com>
2015-03-24 11:23:41 +0000

    cadfael: Add cxx11 option

--- cadfael.rb
+++ cadfael.rb
@@ -2,15 +2,27 @@ class Cadfael < Formula
   url File.dirname(__FILE__), :using => :git
   version "1.0.0"
 
+  option :cxx11
+
   depends_on "drbenmorgan/supernemo/cadfael-toolchain"
 
-  depends_on "boost" => ["with-icu4c", "without-single", "without-static"]
-  #depends_on "boost-python"
-  depends_on "drbenmorgan/supernemo/camp" => "with-doc"
-  depends_on "drbenmorgan/supernemo/clhep"
-  depends_on "drbenmorgan/supernemo/geant4" => "with-gdml"
-  depends_on "drbenmorgan/supernemo/xerces-c"
-  depends_on "drbenmorgan/supernemo/root" => ["with-python"]
+  if build.cxx11?
+    depends_on "boost" => ["c++11", "with-icu4c", "without-single", "without-static"]
+    #depends_on "boost-python"
+    depends_on "drbenmorgan/supernemo/clhep" => "c++11"
+    depends_on "drbenmorgan/supernemo/xerces-c" => "c++11"
+    depends_on "drbenmorgan/supernemo/geant4" => "with-xerces-c"
+    depends_on "drbenmorgan/supernemo/camp" => ["c++11", "with-doxygen"]
+    depends_on "drbenmorgan/supernemo/root" => ["c++11", "with-python"]
+  else
+    depends_on "boost" => ["with-icu4c", "without-single", "without-static"]
+    #depends_on "boost-python"
+    depends_on "drbenmorgan/supernemo/clhep"
+    depends_on "drbenmorgan/supernemo/xerces-c"
+    depends_on "drbenmorgan/supernemo/geant4" => "with-xerces-c"
+    depends_on "drbenmorgan/supernemo/camp" => "with-doxygen"
+    depends_on "drbenmorgan/supernemo/root" => "with-python"
+  end
 
   def install
     bin.mkdir
